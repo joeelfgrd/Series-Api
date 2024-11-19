1069ce89f9f92b0ce5cc0916e629faf6054d8e77
Victor <Victorsancheznogueira@gmail.com>
2024-11-19 22:14:45 +0100

    fix: scripts inserts

--- scripts/datosBBDD.sql
+++ scripts/datosBBDD.sql
@@ -7,24 +7,24 @@ VALUES ('Stranger Things', '2016-07-15', 'Sci-Fi', 'The Duffer Brothers', 8, 'En
 INSERT INTO SERIES (NOMBRE, FECHA_ESTRENO, TEMATICA, DIRECTOR, RATING, IDIOMA, ESTADO, CADENA)
 VALUES ('La Casa de Papel', '2017-05-02', 'Crime', 'Álex Pina', 8, 'Spanish', 'EN EMISION', 'Antena 3');
 
-INSERT INTO Episodios (numero, temporada, nombre, serie, estreno, duracion) VALUES
+INSERT INTO Episodios (numero, temporada, nombre, serie, FECHA_SALIDA, duracion) VALUES
 -- Breaking Bad
-(1, 1, 'Pilot', 'Breaking Bad', '2008-01-20', 58),
-(2, 1, 'Cats in the Bag...', 'Breaking Bad', '2008-01-27', 48),
-(3, 1, '...And the Bags in the River', 'Breaking Bad', '2008-02-10', 47),
-(4, 2, 'Seven Thirty-Seven', 'Breaking Bad', '2009-03-08', 47),
-(5, 2, 'Grilled', 'Breaking Bad', '2009-03-15', 47),
+(1, 1, 'Pilot', 1, '2008-01-20', 58),
+(2, 1, 'Cats in the Bag...', 1, '2008-01-27', 48),
+(3, 1, '...And the Bags in the River', 1, '2008-02-10', 47),
+(4, 2, 'Seven Thirty-Seven', 1, '2009-03-08', 47),
+(5, 2, 'Grilled', 1, '2009-03-15', 47),
 
 -- Stranger Things
-(1, 1, 'The Vanishing of Will Byers', 'Stranger Things', '2016-07-15', 49),
-(2, 1, 'The Weirdo on Maple Street', 'Stranger Things', '2016-07-15', 55),
-(3, 1, 'Holly, Jolly', 'Stranger Things', '2016-07-15', 51),
-(4, 2, 'MADMAX', 'Stranger Things', '2017-10-27', 48),
-(5, 2, 'Trick or Treat, Freak', 'Stranger Things', '2017-10-27', 56),
+(1, 1, 'The Vanishing of Will Byers', 2, '2016-07-15', 49),
+(2, 1, 'The Weirdo on Maple Street', 2, '2016-07-15', 55),
+(3, 1, 'Holly, Jolly', 2, '2016-07-15', 51),
+(4, 2, 'MADMAX', 2, '2017-10-27', 48),
+(5, 2, 'Trick or Treat, Freak', 2, '2017-10-27', 56),
 
 -- La Casa de Papel
-(1, 1, 'Efectuar lo acordado', 'La Casa de Papel', '2017-05-02', 50),
-(2, 1, 'Imprudencias letales', 'La Casa de Papel', '2017-05-09', 41),
-(3, 1, 'Errar al disparar', 'La Casa de Papel', '2017-05-16', 44),
-(4, 2, 'La cabeza del plan', 'La Casa de Papel', '2017-10-16', 47),
-(5, 2, 'A contrarreloj', 'La Casa de Papel', '2017-10-23', 50);
+(1, 1, 'Efectuar lo acordado', 3, '2017-05-02', 50),
+(2, 1, 'Imprudencias letales', 3, '2017-05-09', 41),
+(3, 1, 'Errar al disparar', 3, '2017-05-16', 44),
+(4, 2, 'La cabeza del plan', 3, '2017-10-16', 47),
+(5, 2, 'A contrarreloj', 3, '2017-10-23', 50);
